dace37b1e954305d3ca4129da508c430ef9ae8e8
Rogozan Daniel <Daniel Rogozan>
2018-11-18 10:25:58 +0200

    update run

--- engine/twitter_integration.py
+++ engine/twitter_integration.py
@@ -2,6 +2,8 @@
     connecting, parsing and sending messages on twitter
 """
 import twitter
+from engine.volatil_params import VolatileParams
+from engine.date_formating import DateFormating
 
 
 class TwitterIntegration:
@@ -11,8 +13,7 @@ class TwitterIntegration:
             twitter_api_details
     """
 
-    def __init__(self, connection_details):
-        print(connection_details)
+    def __init__(self, connection_details, users):
         self.consumer_key = connection_details[0]
         self.consumer_secret = connection_details[1]
         self.access_token_key = connection_details[2]
@@ -21,3 +22,18 @@ class TwitterIntegration:
                                consumer_secret=self.consumer_secret,
                                access_token_key=self.access_token_key,
                                access_token_secret=self.access_token_secret)
+        self.users = users
+
+    def twitter_request(self, reboot=False):
+        user_timeline = self.api.GetUserTimeline(screen_name="DRogozan", count=1)
+        print(user_timeline)
+        VolatileParams().change_param("test")
+        print(VolatileParams().get_param("test"))
+
+    def reply(self, msg, status_id=None):
+        user = "@" + self.users
+        answer = user + "\n" + DateFormating("%Y-%m-%d %H:%M").date_now() + "\n" + msg
+        if status_id:
+            self.api.PostUpdate(answer, in_reply_to_status_id=status_id)
+        else:
+            self.api.PostUpdate(answer)
--- run.py
+++ run.py
@@ -6,15 +6,25 @@ from engine.automation_config import AutomationConfig
 from engine.start_logging import StartLogging
 from engine.twitter_integration import TwitterIntegration
 from engine.temperature import Temperature
+from engine.reboot import Reboot
 
 if __name__ == '__main__':
-    CONFIG = AutomationConfig(sys.argv[1])
+    # CONFIG = AutomationConfig(sys.argv[1])
+    CONFIG = AutomationConfig("test.json")
 
+    print('logging start')
     logging = StartLogging(CONFIG.get_param("automationLogDir"), CONFIG.get_param("automationLogFile"))
     logging.start()
 
+    print('twitter start')
     TWITTER_PARRAMS = CONFIG.twitter_api_details()
-    print(TWITTER_PARRAMS)
-    twitter_start = TwitterIntegration(TWITTER_PARRAMS)
+    TWITTER_USERS = CONFIG.get_param("screenUser")
+    tweet = TwitterIntegration(TWITTER_PARRAMS, TWITTER_USERS)
 
-    temperature = Temperature()
+    print('init temperature')
+    temperature = Temperature(CONFIG.get_param("outdoorTemperatureLogFile"))
+
+    print('reboot')
+    tweet.reply(Reboot(CONFIG.get_param("delay_reboot")).start(temperature.check_log_path()))
+
+    tweet.twitter_request()

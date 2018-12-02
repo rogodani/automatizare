85d27ee7b8ccf41746e01d1f23db2e61cb008f9e
Rogozan Daniel <Daniel Rogozan>
2018-12-02 12:17:10 +0200

    add the lights in menu

--- sensors/on_off_relay.py
+++ sensors/on_off_relay.py
@@ -5,15 +5,15 @@ class OnOffRelay:
 
     def __init__(self, gpio_no):
         self.gpio_no = str(gpio_no)
-        os.system("gpio -g mode " + self.gpio_no + " out")
+        os.system("gpio mode " + self.gpio_no + " out")
 
     def relay_on(self):
         print("relay on ----------")
-        os.system("gpio -g write " + self.gpio_no + " 0")
+        os.system("gpio write " + self.gpio_no + " 0")
 
     def relay_off(self):
-        os.system("gpio -g write " + self.gpio_no + " 1")
+        os.system("gpio write " + self.gpio_no + " 1")
 
     def all_lights_off(self, gpios):
         for gpio in gpios:
-            os.system("gpio -g write " + self.gpio_no + " 1")
+            os.system("gpio write " + gpio + " 1")
